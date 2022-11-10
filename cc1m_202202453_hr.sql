fd3b96a746e0959077e02184183f15ed8e25d2a7
eloisapsl <111752882+eloisapsl@users.noreply.github.com>
2022-11-10 11:05:13 -0300

    Add files via upload

--- cc1m_202202453_hr.sql
+++ cc1m_202202453_hr.sql
@@ -1,17 +1,17 @@
 
 CREATE TABLE hr.regioes (
                 id_regiao INTEGER NOT NULL,
-                nome VARCHAR(25) NOT NULL,
+                nome_regiao VARCHAR(25) NOT NULL,
                 CONSTRAINT id_regiao PRIMARY KEY (id_regiao)
 );
 COMMENT ON TABLE hr.regioes IS 'A tabela regiões contém os nomes e os números das regiões em que a empresa atua.';
 COMMENT ON COLUMN hr.regioes.id_regiao IS 'Chave primária da tabela regiões.';
-COMMENT ON COLUMN hr.regioes.nome IS 'Nome da região.';
+COMMENT ON COLUMN hr.regioes.nome_regiao IS 'Nome da região.';
 
 
 CREATE UNIQUE INDEX regioes_idx
  ON hr.regioes
- ( nome );
+ ( nome_regiao );
 
 CREATE TABLE hr.paises (
                 id_pais INTEGER NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE hr.localizacoes (
                 endereco VARCHAR(50),
                 cep VARCHAR(12) NOT NULL,
                 cidade VARCHAR(50),
-                unidade_federal VARCHAR(25),
+                uf  VARCHAR(25),
                 id_pais INTEGER NOT NULL,
                 CONSTRAINT id_localizacao PRIMARY KEY (id_localizacao)
 );
@@ -43,7 +43,7 @@ COMMENT ON COLUMN hr.localizacoes.id_localizacao IS 'Chave primária da tabela l
 COMMENT ON COLUMN hr.localizacoes.endereco IS 'Endereço (número, andar, logradouro) de alguma dependência da empresa.';
 COMMENT ON COLUMN hr.localizacoes.cep IS 'CEP da localização de algum escritório ou facilidade da empresa.';
 COMMENT ON COLUMN hr.localizacoes.cidade IS 'Cidade onde o escritório ou outra facilidade da empresa está localizado.';
-COMMENT ON COLUMN hr.localizacoes.unidade_federal IS 'Abreviação para Unidade Federal. Estado onde o escritório ou facilidade da empresa está localizado.';
+COMMENT ON COLUMN hr.localizacoes.uf  IS 'Abreviação para Unidade Federal. Estado onde o escritório ou facilidade da empresa está localizado.';
 COMMENT ON COLUMN hr.localizacoes.id_pais IS 'Chave estrangeira que referencia a tabela países.';
 
 
@@ -102,35 +102,35 @@ CREATE UNIQUE INDEX email
 CREATE TABLE hr.departamentos (
                 id_departamento INTEGER NOT NULL,
                 id_localizacao INTEGER NOT NULL,
-                nome VARCHAR(50),
+                nome_dpt VARCHAR(50),
                 id_gerente INTEGER NOT NULL,
                 CONSTRAINT id_departamento PRIMARY KEY (id_departamento)
 );
 COMMENT ON TABLE hr.departamentos IS 'A tabela departamentos armazena os nomes dos departamentos da empresa e suas respectivas informações.';
 COMMENT ON COLUMN hr.departamentos.id_departamento IS 'Chave primária da tabela departamentos.';
 COMMENT ON COLUMN hr.departamentos.id_localizacao IS 'Chave estrangeira que tem como referência a tabela de localizações.';
-COMMENT ON COLUMN hr.departamentos.nome IS 'Nome do departamento.';
+COMMENT ON COLUMN hr.departamentos.nome_dpt IS 'Nome do departamento.';
 COMMENT ON COLUMN hr.departamentos.id_gerente IS 'Chave estrangeira que tem como referência a tabela empregados. Representa qual empregado, se houver, é o gerente deste departamento.';
 
 
 CREATE UNIQUE INDEX departamentos_idx
  ON hr.departamentos
- ( nome );
+ ( nome_dpt );
 
 CREATE TABLE hr.historico_cargos (
+                id_funcionario INTEGER NOT NULL,
                 data_inicial DATE NOT NULL,
-                id_empregado INTEGER NOT NULL,
                 data_final DATE NOT NULL,
                 id_departamento INTEGER NOT NULL,
-                id_cargo INTEGER NOT NULL,
-                CONSTRAINT id_empregado PRIMARY KEY (data_inicial, id_empregado)
+                id_cargof INTEGER NOT NULL,
+                CONSTRAINT id_empregado PRIMARY KEY (id_funcionario, data_inicial)
 );
 COMMENT ON TABLE hr.historico_cargos IS 'A tabela armazena o histórico de cargos de um empregado. Caso ele mude de cargo dentro de um departamento ou mude de departamento dentro de um cargo, essas informações devem ser registradas na tabela.';
+COMMENT ON COLUMN hr.historico_cargos.id_funcionario IS 'Junto a primary key data_inicial, forma a chave primária composta da tabela. Também é uma chave estrangeira que tem como referência a tabela empregados.';
 COMMENT ON COLUMN hr.historico_cargos.data_inicial IS 'Junto a primary key id_empregados, forma a chave primária composta da tabela. Indica a data de inicio de um empregado em um novo cargo. Deve ser menor que a data_final.';
-COMMENT ON COLUMN hr.historico_cargos.id_empregado IS 'Junto a primary key data_inicial, forma a chave primária composta da tabela. Também é uma chave estrangeira que tem como referência a tabela empregados.';
 COMMENT ON COLUMN hr.historico_cargos.data_final IS 'Ultimo dia do empregado no cargo. Deve ser maior que a data_inicial.';
 COMMENT ON COLUMN hr.historico_cargos.id_departamento IS 'Chave estrangeira que referencia a tabela departamentos. Corresponde ao antigo departamento em que o empregado trabalhava.';
-COMMENT ON COLUMN hr.historico_cargos.id_cargo IS 'Chave estrangeira que tem como referência a tabela cargos. Corresponde ao cargo em que o empregado estava trabalhando no passado.';
+COMMENT ON COLUMN hr.historico_cargos.id_cargof IS 'Chave estrangeira que tem como referência a tabela cargos. Corresponde ao cargo em que o empregado estava trabalhando no passado.';
 
 
 ALTER TABLE hr.paises ADD CONSTRAINT regioes_paises_fk
@@ -155,7 +155,7 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 ALTER TABLE hr.historico_cargos ADD CONSTRAINT cargos_historico_cargos_fk
-FOREIGN KEY (id_cargo)
+FOREIGN KEY (id_cargof)
 REFERENCES hr.cargos (id_cargo)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
@@ -169,7 +169,7 @@ ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
 ALTER TABLE hr.historico_cargos ADD CONSTRAINT empregados_historico_cargos_fk
-FOREIGN KEY (id_empregado)
+FOREIGN KEY (id_funcionario)
 REFERENCES hr.empregados (id_empregado)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
